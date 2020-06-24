d920c76b2d00fe0be702ec1493bcd871ce847fe7
maxim.xprt <maxim.xprt@gmail.com>
2020-06-24 13:43:10 +0300

    Readme extention

--- air_project_parallels_dag.py
+++ air_project_parallels_dag.py
@@ -3,10 +3,10 @@ from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
 
 from datetime import timedelta
-import requests
-import re
-import csv
-import os
+# import requests
+# import re
+# import csv
+# import os
 
 import air_project as libs
 import secur.credentials as ENV
@@ -16,7 +16,7 @@ PARTS_NUMBER = 4
 
 args = {
     'owner': 'air101',
-    'start_date': days_ago(2),
+    'start_date': days_ago(1),
     'retries': 0,
     'retry_delay': timedelta(minutes=1),
 }
@@ -32,10 +32,6 @@ def sla_miss_action(*args, **kwargs):
     libs.bot_message(message_text=alert_text)
 
 
-# def load_links_from_gsheet(gsheet_url: str, stage_filename: str) -> None:
-#     libs.write_list_to_csv(['url'], \
-#         libs.get_url_from_gsheet(table_url=gsheet_url, auth_json_file=ENV.GSHEET_KEY_FILE), stage_filename)
-
 def load_links_from_gsheet(gsheet_url: str, stage_filename: str) -> None:
     gsheet_urls = libs.get_url_from_gsheet(table_url=gsheet_url,
                                            auth_json_file=ENV.GSHEET_KEY_FILE,
@@ -43,8 +39,6 @@ def load_links_from_gsheet(gsheet_url: str, stage_filename: str) -> None:
 
     libs.write_gheet_data_with_parts(
         gsheet_urls, parts=PARTS_NUMBER, parent_file_name=stage_filename)
-    # libs.write_list_to_csv(['url'],
-    #                        libs.get_url_from_gsheet(table_url=gsheet_url, auth_json_file=ENV.GSHEET_KEY_FILE), stage_filename)
 
 
 def parse_links_watchers(stage_filename: str, result_filename: str,
@@ -55,7 +49,7 @@ def parse_links_watchers(stage_filename: str, result_filename: str,
 
 with DAG(dag_id='air101_project_with_parts',
          default_args=args,
-         schedule_interval=timedelta(days=2),
+         schedule_interval=timedelta(days=1),
          sla_miss_callback=sla_miss_action,
          on_failure_callback=on_failure_action,
          ) as dag:
@@ -79,13 +73,6 @@ with DAG(dag_id='air101_project_with_parts',
                            'part': part+1}
             )
         )
-    # parse_links_watchers = PythonOperator(
-    #     task_id='parse_links_watchers',
-    #     python_callable=parse_links_watchers,
-    #     # provide_context=True,
-    #     op_kwargs={'stage_filename': ENV.UPLOADED_GSHEET_FILE,\
-    #                'result_filename': ENV.PARSED_DATA_SET_FILE}
-    # )
 
     write_to_gsheet = PythonOperator(
         trigger_rule='all_done',
@@ -102,4 +89,3 @@ with DAG(dag_id='air101_project_with_parts',
     )
 
     load_links_from_gsheet >> parse_links_watchers_list >> write_to_gsheet >> send_report
-    # load_links_from_gsheet >> parse_links_watchers >> write_to_gsheet >> send_report
