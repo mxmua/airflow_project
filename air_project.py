d5e343b7dc3814b8d91f2e6c43c6c7a234b97a2c
Dmitry <dimk00z@gmail.com>
2020-06-26 11:08:05 +0300

    Revert "chrome_options"
    This reverts commit 848f581c20e298dbb1c02e3ec487cc1c9f04669e.

--- air_project.py
+++ air_project.py
@@ -122,15 +122,6 @@ def create_browser(url):
     # sudo apt install chromium-chromedriver
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    options.add_argument("start-maximized")
-    options.add_argument("enable-automation")
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-browser-side-navigation")
-    options.add_argument("--dns-prefetch-disable")
-    options.add_argument("--disable-gpu")
     browser = webdriver.Chrome(options=options)
     browser.get(url)
     return browser
